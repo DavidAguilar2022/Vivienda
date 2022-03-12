b175cc7be07742d55ea8ab4f8783da0135a7cc99
David Aguilar Almagro <>
2022-03-12 16:21:19 +0100

    Documentación realizada

--- Vivienda/src/Vivienda.java
+++ Vivienda/src/Vivienda.java
@@ -7,9 +7,12 @@
 /**
  *
  * @author Paqui Elena
+ * @version 2.0
  */
 public class Vivienda 
 {
+    //campos de la clase
+    
     private double precio;
     private int numHabitaciones;
     private double superficie;
@@ -17,12 +20,29 @@ public class Vivienda
     private String estado;
     private String propietario;
     
+    /**
+     * constructor que solo recibe el parámetro precio 
+     * @param precio 
+     * 
+     */
     public Vivienda (double precio)
     {
         this.precio = precio;
     }
+    
+    /**
+     * Segundo constructor que recibe como parámetros todos los atributos
+     * @param precio
+     * @param numHabitaciones
+     * @param superficie
+     * @param parking
+     * @param estado
+     * @param propietario 
+     * 
+     */
 
     public Vivienda(double precio, int numHabitaciones, double superficie, boolean parking, String estado, String propietario) {
+        
         this.precio = precio;
         this.numHabitaciones = numHabitaciones;
         this.superficie = superficie;
@@ -31,56 +51,110 @@ public class Vivienda
         this.propietario = propietario;
     }
     
+    /**
+     * metodo que actualiza el precio por medio del descuento aplicado
+     * @param descuento 
+     * 
+     */
     
     public void actualizarPrecio(double descuento)
     {
         precio = precio - descuento;
     }
 
+    /**
+     * getter de precio
+     * @return double
+     * 
+     */
     public double getPrecio() {
         return precio;
     }
-
+    /**
+     * setter de precio
+     * @param precio 
+     * 
+     */
     public void setPrecio(double precio) {
         this.precio = precio;
     }
-
+    /**
+     * getter de numHabitaciones
+     * @return int
+     * 
+     */
     public int getNumHabitaciones() {
         return numHabitaciones;
     }
-
+    /**
+     * setter del numHabitaciones
+     * @param numHabitaciones 
+     * 
+     */
     public void setNumHabitaciones(int numHabitaciones) {
         this.numHabitaciones = numHabitaciones;
     }
-
+    /**
+     * getter de superficie
+     * @return superficie
+     * 
+     */
     public double getSuperficie() {
         return superficie;
     }
-
+    /**
+     * setter de superficie
+     * @param superficie 
+     * 
+     */
     public void setSuperficie(double superficie) {
         this.superficie = superficie;
     }
-
+    /**
+     * getter de parking
+     * @return boolean
+     * 
+     */
     public boolean isParking() {
         return parking;
     }
-
+    /**
+     * setter de parking
+     * @param parking 
+     * 
+     */
     public void setParking(boolean parking) {
         this.parking = parking;
     }
-
+    /**
+     * getter de String
+     * @return String
+     * 
+     */
     public String getEstado() {
         return estado;
     }
-
+    /**
+     * setter de estado
+     * @param estado 
+     * 
+     */
     public void setEstado(String estado) {
         this.estado = estado;
     }
-
+    /**
+     * getter de propietario
+     * @return string
+     * 
+     */
     public String getPropietario() {
         return propietario;
     }
-
+    /**
+     * setter propietario
+     * @param propietario
+     * 
+     */
     public void setPropietario(String propietario) {
         this.propietario = propietario;
     }
